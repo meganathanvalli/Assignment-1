51f345455333ea3b909be3e095b2a8f998fc7e5e
Meganathan <114504572+meganathanvalli@users.noreply.github.com>
2022-10-05 17:45:16 +0530

    Create print a number.java

---
+++ print a number.java
@@ -0,0 +1,15 @@
+package print a number;
+
+import java.util.Scanner;
+
+public class print a number {
+
+	public static void main(String[] args) {
+	  Scanner reader=new Scanner(System.in);
+	  System.out.print("Enter a number:");
+	  int number=reader.nextInt();
+	  System.out.println("You entered:"+number);
+
+	}
+
+}
