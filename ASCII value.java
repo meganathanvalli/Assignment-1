d2d6b044357c266dad2d99342507be24b4f1fadc
Meganathan <114504572+meganathanvalli@users.noreply.github.com>
2022-10-05 17:48:56 +0530

    Create ASCII value.java

---
+++ ASCII value.java
@@ -0,0 +1,14 @@
+package ASCII value;
+
+public class ASCII value {
+
+	public static void main(String[] args) {
+		char ch = 'H'; 
+		 int ascii = ch; 
+		 int castAscii = (int) ch; 
+		 System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+		 System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+
+	}
+
+}
