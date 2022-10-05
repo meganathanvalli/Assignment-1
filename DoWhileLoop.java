a94a7cd54b9eba1b613e8712e9fb3cd886ae17ad
Meganathan <114504572+meganathanvalli@users.noreply.github.com>
2022-10-05 16:25:37 +0530

    Create DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,39 @@
+package DoWhileLoop;
+
+public class DoWhlieLoop {
+
+	public static void main(String[] args[]) {
+		int x = 1; 
+		 
+		 do { 
+		 System.out.print("value of x : " + x ); 
+		 x++; 
+		 System.out.print("\n"); 
+		 }while( x < 11 );
+
+	}
+
+}
+
+
+
+
+  
+  
+  
+
+
+		
+
+		 
+
+		
+
+	
+
+	
+
+	
+
+	
+
