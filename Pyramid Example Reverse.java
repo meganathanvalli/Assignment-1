4318ae25221a053e16216174b2ff3bd6d40d7da5
Meganathan <114504572+meganathanvalli@users.noreply.github.com>
2022-10-05 17:57:12 +0530

    Create Pyramid Example Reverse.java

---
+++ Pyramid Example Reverse.java
@@ -0,0 +1,19 @@
+package pyramid Example Reverse;
+
+public class Pyramid Example Reverse {
+
+	public static void main(String[] args) {
+		int term=6; 
+		 
+		for(int i=1;i<=term;i++) 
+		 { 
+		 
+		for(int j=term;j>=i;j--) 
+		 { 
+		 System.out.print("* "); 
+		 } 
+		 System.out.println();
+
+	}
+	}
+}
