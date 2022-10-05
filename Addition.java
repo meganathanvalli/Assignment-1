5dfa274a30570e3be17bdc684a9c756c8382548d
Meganathan <114504572+meganathanvalli@users.noreply.github.com>
2022-10-05 16:07:19 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package Addition;
+
+public class Addition {
+
+	public static void main(String[] args) {		System.out.println("Enter two number");
+
+	  int first=100;
+
+	  int second=200;
+
+	  System.out.println(first+" "+second);
+
+	  int sum=first+second;
+
+	  System.out.println("The sum is:"+sum);
+
+	}
